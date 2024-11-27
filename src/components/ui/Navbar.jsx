7a6b99841671f76d556a7032730cbcd1917423db
ashraful malik <ashrafulmalik71@gmail.com>
2024-11-27 10:09:58 +0530

    chnage name in navbar

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <nav className="bg-dark-800 p-2 ">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white text-2xl font-bold">JourneySkills</div>
+        <div className="text-white text-2xl font-bold">JourneySkill</div>
       </div>
     </nav>
   );
